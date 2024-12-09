94a06d6a8c02f1a9830f44f47cb1a5dcede8b1a8
Olivier Lamothe <olamothe@coveo.com>
2024-12-09 14:09:58 -0500

    fix image aspect ratio

--- app/components/Products/ProductCard.tsx
+++ app/components/Products/ProductCard.tsx
@@ -32,7 +32,7 @@ export function ProductCard({product, onSelect}: ProductCardProps) {
         height={1024}
         alt={product.ec_name!}
         src={product.ec_images[0]}
-        className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
+        className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75"
       />
       <h3 className="mt-4 text-sm text-gray-700">{product.ec_name}</h3>
       <div className="flex">
