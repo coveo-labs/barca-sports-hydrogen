86c359ec2fda62a58ae057a5400c9394fb243c2f
omariwallace <owallace@coveo.com>
2024-12-12 21:48:55 -0500

    add cart selectors

--- app/components/Cart/CartMain.tsx
+++ app/components/Cart/CartMain.tsx
@@ -219,7 +219,7 @@ export function CartMain({cart: originalCart}: CartMainProps) {
                 })
               }
               to={cart?.checkoutUrl || ''}
-              className="block w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+              className="checkout block w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
             >
               Checkout
             </NavLink>
