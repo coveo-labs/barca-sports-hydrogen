89f78c7dd1bdc9049289c17d4f6d84a64aa14ddd
omariwallace <owallace@coveo.com>
2024-12-19 18:18:40 -0500

    add recs selectors to cart page

--- app/components/Cart/CartRecommendations.tsx
+++ app/components/Cart/CartRecommendations.tsx
@@ -4,14 +4,21 @@ import {ProductCard} from '../Products/ProductCard';
 export function CartRecommendations() {
   const recs = useCartRecommendations();
   return (
-    <section aria-labelledby="related-heading" className="mt-24">
-      <h2 id="related-heading" className="text-lg font-medium text-gray-900">
+    <section
+      aria-labelledby="related-heading"
+      className="recommendation-container mt-24"
+    >
+      <h2
+        id="related-heading"
+        className="recommendation-title text-lg font-medium text-gray-900"
+      >
         {recs.state.headline}
       </h2>
 
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div className="recommendation-list mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {recs.state.products.map((relatedProduct) => (
           <ProductCard
+            className="recommendation-card"
             onSelect={
               recs.methods?.interactiveProduct({
                 options: {product: relatedProduct},
