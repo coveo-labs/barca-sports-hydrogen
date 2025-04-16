9c06f0f7dab2e76a8989d5d556bb01380b77a189
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-04-16 14:22:12 -0400

    cleanup

--- app/lib/fetch-token.ts
+++ app/lib/fetch-token.ts
@@ -9,8 +9,6 @@ export const fetchToken = async (request?: null|Request, apiKeyAuthentication = 
     return 'xx697404a7-6cfd-48c6-93d1-30d73d17e07a'; // demo API key
   }
 
-  console.log ('fetching token from', `${baseUrl}/token`);
-  console.log('with headers', request?.headers.get('Cookie'));
   const response = await fetch(`${baseUrl}/token`);
   if (!response.ok) {
     throw new Error(`Failed to fetch token: ${response.status} ${response.statusText}`);
