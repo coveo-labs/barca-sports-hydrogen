2ac777d99a3c25bfc8a225fce8d15420b71ef8f6
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-04-16 11:55:57 -0400

    test fix

--- app/lib/fetch-token.ts
+++ app/lib/fetch-token.ts
@@ -3,7 +3,7 @@ type TokenResponse = {
 };
 
 export const fetchToken = async (request: Request, apiKeyAuthentication = false) => {
-  const baseUrl = request ? new URL(request.url).origin : '';
+  const baseUrl = request && request.url ? new URL(request.url).origin : '';
 
   return apiKeyAuthentication
     ? 'xx697404a7-6cfd-48c6-93d1-30d73d17e07a' // demo api key
