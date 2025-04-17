0d670bce369a326b9dc88abf8ebe996e16545852
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2025-04-16 21:00:56 -0400

    try relaying headers

--- app/lib/fetch-token.ts
+++ app/lib/fetch-token.ts
@@ -2,17 +2,31 @@ type TokenResponse = {
   token: string;
 };
 
-export const fetchToken = async (request?: null|Request, apiKeyAuthentication = false) => {
+export const fetchToken = async (request?: null | Request, apiKeyAuthentication = false) => {
   const baseUrl = request && request.url ? new URL(request.url).origin : '';
 
   if (apiKeyAuthentication || typeof window == 'undefined') {
     return 'xx697404a7-6cfd-48c6-93d1-30d73d17e07a'; // demo API key
   }
 
-  const response = await fetch(`${baseUrl}/token`);
+  const headers = new Headers();
+  if (request) {
+    // Relay headers from the incoming request
+    const authHeader = request.headers.get('Authorization');
+    if (authHeader) {
+      headers.set('Authorization', authHeader);
+    }
+
+    const cookieHeader = request.headers.get('Cookie');
+    if (cookieHeader) {
+      headers.set('Cookie', cookieHeader);
+    }
+  }
+
+  const response = await fetch(`${baseUrl}/token`, { headers });
   if (!response.ok) {
     throw new Error(`Failed to fetch token: ${response.status} ${response.statusText}`);
   }
-  const data = await response.json() as TokenResponse;
+  const data = (await response.json()) as TokenResponse;
   return data.token;
 };
