c4b1f1d65426798ffdefecfdeafad75c78608ed3
Olivier Lamothe <olamothe@coveo.com>
2025-04-24 15:49:00 -0400

    use createCookie from remix-oxygen

--- app/lib/token-utils.server.ts
+++ app/lib/token-utils.server.ts
@@ -1,14 +1,6 @@
-import { parse } from 'cookie';
-import { engineDefinition } from './coveo.engine';
-import { fetchToken } from '~/lib/fetch-token';
-import { createCookie } from "@remix-run/node";
-
-export const accessTokenCookie = createCookie('coveo_accessToken', {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
-  path: '/',
-});
+import {engineDefinition} from './coveo.engine';
+import {fetchToken} from '~/lib/fetch-token';
+import {accessTokenCookie} from './cookies.server';
 
 export function decodeBase64Url(base64Url: string): string {
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
@@ -16,13 +8,15 @@ export function decodeBase64Url(base64Url: string): string {
 }
 
 export function isTokenExpired(token: string): boolean {
-    if (isApiKey(token)) {
-        return false;
-    }
+  if (isApiKey(token)) {
+    return false;
+  }
 
   try {
     const [, payload] = token.split('.');
-    const decodedPayload = JSON.parse(decodeBase64Url(payload)) as { exp: number };
+    const decodedPayload = JSON.parse(decodeBase64Url(payload)) as {
+      exp: number;
+    };
     return decodedPayload.exp * 1000 < Date.now();
   } catch {
     return true; // Treat invalid tokens as expired
@@ -31,13 +25,8 @@ export function isTokenExpired(token: string): boolean {
 
 function isApiKey(token: string) {
   return /^xx[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
-    token
+    token,
   );
-}
-
-export function extractAccessTokenFromCookie(request: Request): string | null {
-  const cookies = parse(request.headers.get('Cookie') ?? '');
-  return cookies['coveo_accessToken'];
 }
 
 export async function updateTokenIfNeeded(
@@ -46,14 +35,17 @@ export async function updateTokenIfNeeded(
     | 'searchEngineDefinition'
     | 'standaloneEngineDefinition'
     | 'recommendationEngineDefinition',
-  request: Request)
-  {
-    if (isTokenExpired(engineDefinition[engineType].getAccessToken())) {
-      const accessTokenCookie = extractAccessTokenFromCookie(request)
-      const accessToken =  accessTokenCookie && !isTokenExpired(accessTokenCookie)
-        ? accessTokenCookie
+  request: Request,
+) {
+  if (isTokenExpired(engineDefinition[engineType].getAccessToken())) {
+    const accessTokenCookieValue = await accessTokenCookie.parse(
+      request.headers.get('Cookie'),
+    );
+    const accessToken =
+      accessTokenCookieValue && !isTokenExpired(accessTokenCookieValue)
+        ? accessTokenCookieValue
         : await fetchToken(request);
 
-        engineDefinition[engineType].setAccessToken(accessToken);
-    }
+    engineDefinition[engineType].setAccessToken(accessToken);
   }
+}
