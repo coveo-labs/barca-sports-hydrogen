1be38a5f73b284036222e72626d09f0fc4f7903e
JDevost <jdevost@coveo.com>
2025-12-12 16:27:29 -0500

    hide size label when empty

--- app/components/Products/Sizes.tsx
+++ app/components/Products/Sizes.tsx
@@ -17,6 +17,13 @@ export function Sizes({
       ?.value || 'Medium',
   );
 
+  const sizeValues =
+    product.options.find((opt) => opt.name === 'Size')?.optionValues || [];
+
+  if (sizeValues.length === 0) {
+    return null;
+  }
+
   return (
     <div className="mt-8">
       <div className="flex items-center justify-between">
@@ -29,19 +36,17 @@ export function Sizes({
           onChange={setSelectedSize}
           className="grid grid-cols-3 gap-3 sm:grid-cols-6"
         >
-          {product.options
-            .find((opt) => opt.name === 'Size')
-            ?.optionValues.map(({name: size}) => (
-              <Radio
-                key={size}
-                value={size}
-                className={
-                  'cursor-pointer focus:outline-none flex items-center justify-center rounded-md border border-gray-200 bg-white px-3 py-3 text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 data-[checked]:border-transparent data-[checked]:bg-indigo-600 data-[checked]:text-white data-[focus]:ring-2 data-[focus]:ring-indigo-500 data-[focus]:ring-offset-2 data-[checked]:hover:bg-indigo-700 sm:flex-1'
-                }
-              >
-                {size}
-              </Radio>
-            ))}
+          {sizeValues.map(({name: size}) => (
+            <Radio
+              key={size}
+              value={size}
+              className={
+                'cursor-pointer focus:outline-none flex items-center justify-center rounded-md border border-gray-200 bg-white px-3 py-3 text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 data-[checked]:border-transparent data-[checked]:bg-indigo-600 data-[checked]:text-white data-[focus]:ring-2 data-[focus]:ring-indigo-500 data-[focus]:ring-offset-2 data-[checked]:hover:bg-indigo-700 sm:flex-1'
+              }
+            >
+              {size}
+            </Radio>
+          ))}
         </RadioGroup>
       </fieldset>
     </div>
